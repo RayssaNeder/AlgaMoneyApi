bfde0a94564dff0bae4b7b26d3bc0bd500ba1576
Rayssa Neder <rayssafreitas13@gmail.com>
2020-09-13 15:28:37 -0300

    3.10. Validando valores inválidos com Bean Validation

--- algamoney-api/src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyException.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyException.java
@@ -1,5 +1,8 @@
 package com.example.algamoney.api.exceptionhandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -25,11 +31,34 @@ public class AlgamoneyException extends ResponseEntityExceptionHandler {
 		
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
 		String mensagemDesenvolvedor = ex.getCause().toString();
-		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
 	}
 	
 	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<Erro> errors = createErrorList(ex.getBindingResult());
+		
+		return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
+	}
 	
+	
+	private List<Erro> createErrorList (BindingResult bindingResult){
+		List<Erro> errors = new ArrayList<Erro>();
+		
+		for(FieldError fieldError : bindingResult.getFieldErrors()) {
+			
+			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());;
+			String mensagemDesenvolvedor = fieldError.toString();;
+			
+			errors.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		}
+		
+		
+		return errors;
+	}
 	
 	public static class Erro{
 		private String mensagemUsuario;
--- algamoney-api/src/main/java/com/example/algamoney/api/model/Categoria.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "categoria")
@@ -13,6 +15,8 @@ public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codigo;
+	@NotNull
+	@Size(max = 3, min = 20)
 	private String nome;
 	public Long getCodigo() {
 		return codigo;
